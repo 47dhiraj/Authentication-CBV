6f3f4b9f1ba5071236f2404f817697ef03e24b83
dhirajkafle553 <dhirajkafle553@gmail.com>
2022-04-24 22:59:16 +0545

    Complete custom authentication using Class Based Views

---
+++ auth_app/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth.decorators import login_required
+
+
+urlpatterns = [
+
+    path('home/', login_required(views.HomeView.as_view()), name='home'),
+
+    path('register/', views.RegistrationView.as_view(), name='register'),
+
+    path('', views.LoginView.as_view(), name='login'),
+
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    
+    path('activate/<slug:uidb64>/<slug:token>/', views.ActivateAccountView.as_view(), name='activate'),
+
+    path('request_reset_email/', views.RequestResetEmailView.as_view(), name='reset_email'),
+    path('set_new_password/<slug:uidb64>/<slug:token>/', views.SetNewPasswordView.as_view(), name='set_new_password'),
+
+    path('accounts/', include('allauth.urls')),
+
+]
